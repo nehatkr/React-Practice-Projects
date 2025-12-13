89c36ae2d6aac237a885fee0b57afde00d89796e
Nehatkr <nehathakur2308@gmail.com>
2025-12-13 15:35:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './component/Navbar'
+import News from './component/page/News'
+
+function App() {
+
+  return (
+    <>
+     <Navbar />
+     <News />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/page/News.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from '../Wrapper'
+
+const News = () => {
+  return (
+    <Wrapper>
+    <div>
+      News
+    </div>
+    </Wrapper>
+  )
+}
+
+export default News
